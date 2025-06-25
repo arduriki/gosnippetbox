dfc514283be346789b354510051668ea96fde785
arduriki <jordi.ardura.professional@gmail.com>
2025-06-25 18:57:50 +0200

    Routing requests

--- main.go
+++ main.go
@@ -5,18 +5,32 @@ import (
 	"net/http"
 )
 
-// home handler which writes a byte slice containing a response body.
+// Add home handler which writes a byte slice containing a response body.
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Add a snippetView handler function to see the created snippets.
+func snippetView(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Display a specific snippet..."))
+}
+
+// Add a snippetCreate handler function.
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Display a form for creating a new snippet..."))
 }
 
 func main() {
 	// Initialize a new servemux, then register the home function
 	// as a handler for the "/" URL pattern.
-    // Careful: it'll act as a 'catch-all' of any URL path
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-
+    // {$} to prevent subtree path, it helps to match a single slash with
+    // nothing else.
+	mux.HandleFunc("/{$}", home)
+    // Register view and create with their corresponding route patterns.
+    mux.HandleFunc("/snippet/view", snippetView)
+    mux.HandleFunc("/snippet/create", snippetCreate)
+    
 	// Print a log message to say that the server is starting.
 	log.Print("starting server on :4000")
 
