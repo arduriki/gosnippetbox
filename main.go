a65f9151d2f425d715474a5c90cebe130012fc08
arduriki <jordi.ardura.professional@gmail.com>
2025-06-25 20:54:10 +0200

    Method based routing implemented

--- main.go
+++ main.go
@@ -34,16 +34,23 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Display a form for creating a new snippet..."))
 }
 
+// Add a snippetCreatePost handler function.
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Save a new snippet..."))
+}
+
 func main() {
 	// Initialize a new servemux, then register the home function
 	// as a handler for the "/" URL pattern.
 	mux := http.NewServeMux()
     // {$} to prevent subtree path, it helps to match a single slash with
     // nothing else.
-	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("GET /{$}", home) // GET to restrict the HTTP method.
     // Register view and create with their corresponding route patterns.
-    mux.HandleFunc("/snippet/view/{id}", snippetView) // {id} is a wildcard
-    mux.HandleFunc("/snippet/create", snippetCreate)
+    mux.HandleFunc("GET /snippet/view/{id}", snippetView) // {id} is a wildcard.
+    mux.HandleFunc("GET /snippet/create", snippetCreate)
+	// A POST route request only.
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
     
 	// Print a log message to say that the server is starting.
 	log.Print("starting server on :4000")
