0d544aaf9350e472902843b5cc45c0b7566dce2d
arduriki <jordi.ardura.professional@gmail.com>
2025-06-25 20:37:45 +0200

    wildcard route patterns

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Add home handler which writes a byte slice containing a response body.
@@ -12,7 +14,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetView handler function to see the created snippets.
 func snippetView(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Display a specific snippet..."))
+	// Extract the value of the id wildcard from the request
+	// and try to convert it to an integer.
+	id, err := strconv.Atoi(r.PathValue("id"))
+	// If it can't be converted to an integer, or the value is less than 1,
+	// return 404 page not found.
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+    
+	// A message with the id value as the HTTP response.
+	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+	w.Write([]byte(msg))
 }
 
 // Add a snippetCreate handler function.
@@ -28,7 +42,7 @@ func main() {
     // nothing else.
 	mux.HandleFunc("/{$}", home)
     // Register view and create with their corresponding route patterns.
-    mux.HandleFunc("/snippet/view", snippetView)
+    mux.HandleFunc("/snippet/view/{id}", snippetView) // {id} is a wildcard
     mux.HandleFunc("/snippet/create", snippetCreate)
     
 	// Print a log message to say that the server is starting.
